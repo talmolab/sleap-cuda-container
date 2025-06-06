02d16546213c009d50612915a5399c23dde8bd82
7174Andy <hep003@ucsd.edu>
2025-06-05 18:47:46 -0700

    modify start up script to clone

--- lablink_sleap_client_image/start.sh
+++ lablink_sleap_client_image/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo "Starting the lablink sleap client container..."
+
+echo "ALLOCATOR_HOST: $ALLOCATOR_HOST"
+echo "TUTORIAL_REPO_TO_CLONE: $TUTORIAL_REPO_TO_CLONE"
+
+if [ -n "$TUTORIAL_REPO_TO_CLONE" ]; then
+  cd /home/client/Desktop
+  echo "Cloning repository $TUTORIAL_REPO_TO_CLONE..."
+  sudo -u client git clone "$TUTORIAL_REPO_TO_CLONE"
+else
+  echo "TUTORIAL_REPO_TO_CLONE not set. Skipping clone step."
+fi
+
 echo "Running subscribe script..."
 
 # Activate the conda environment and run the subscribe script
