b5ba1214c5957ea2cbec3fd5d8d238ce0970eb8b
7174Andy <hep003@ucsd.edu>
2025-06-06 10:51:47 -0700

    remove miniforge

--- lablink_sleap_client_image/start.sh
+++ lablink_sleap_client_image/start.sh
@@ -10,6 +10,12 @@ if [ -n "$TUTORIAL_REPO_TO_CLONE" ]; then
   cd /home/client/Desktop
   echo "Cloning repository $TUTORIAL_REPO_TO_CLONE..."
   sudo -u client git clone "$TUTORIAL_REPO_TO_CLONE"
+  if [$? -ne 0]; then
+    echo "Failed to clone repository $TUTORIAL_REPO_TO_CLONE"
+    exit 1
+  else
+    echo "Successfully cloned repository $TUTORIAL_REPO_TO_CLONE"
+  fi
 else
   echo "TUTORIAL_REPO_TO_CLONE not set. Skipping clone step."
 fi
@@ -17,7 +23,7 @@ fi
 echo "Running subscribe script..."
 
 # Activate the conda environment and run the subscribe script
-/home/client/miniforge3/bin/conda run -n base subscribe allocator.host=$ALLOCATOR_HOST
+subscribe allocator.host=$ALLOCATOR_HOST
 
 # Keep the container alive
 tail -f /dev/null
